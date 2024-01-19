16b7f2fbafeaa607987d1ec52ce0a776a2d69c11
Man Nguyen <nnman.vn@gmail.com>
2024-01-20 01:42:40 +0800

    fix: loading indicator and open burn dialog

--- assets/index.js
+++ assets/index.js
@@ -16,6 +16,30 @@ function listenCloseMobileNav() {
     });
 }
 
+function openLoading() {
+  document.getElementById("loading-indicator").classList.remove("hidden");
+  document.getElementById("loading-indicator").classList.add("flex");
+  document.body.style.overflow = "hidden";
+}
+
+function closeLoading() {
+  document.getElementById("loading-indicator").classList.add("hidden");
+  document.getElementById("loading-indicator").classList.remove("flex");
+  document.body.style.overflow = "auto";
+}
+
+function openBurnCardDialog() {
+  document.getElementById("burn-card-dialog").classList.remove("hidden");
+  document.getElementById("burn-card-dialog").classList.add("flex");
+  document.body.style.overflow = "hidden";
+}
+
+function closeBurnCardDialog() {
+  document.getElementById("burn-card-dialog").classList.add("hidden");
+  document.getElementById("burn-card-dialog").classList.remove("flex");
+  document.body.style.overflow = "auto";
+}
+
 function init() {
   listenOpenMobileNav();
   listenCloseMobileNav();
